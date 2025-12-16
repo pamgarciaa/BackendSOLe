9035ca7c02003278ce7cdfb764a869c24fbe32ec
PAMELA <pamelaramosgarcia93@outlook.es>
2025-12-16 19:48:15 +0100

    edit kit model

--- src/models/kit.model.ts
+++ src/models/kit.model.ts
@@ -3,10 +3,9 @@ import mongoose, { Document, Schema, Model } from "mongoose";
 export interface IKit extends Document {
     name: string;
     description: string;
-    price: number;
+    price?: number;
     image: string;
     features: string[];
-    level: number;
     isDigital: boolean;
     createdAt: Date;
     updatedAt: Date;
@@ -16,10 +15,9 @@ const kitSchema = new Schema<IKit>(
     {
         name: { type: String, required: true, trim: true },
         description: { type: String, required: true },
-        price: { type: Number, required: true, min: 0 },
+        price: { type: Number, required: false, min: 0 },
         image: { type: String, required: true },
         features: [{ type: String }],
-        level: { type: Number, required: true },
         isDigital: { type: Boolean, default: true },
     },
     { timestamps: true, versionKey: false }
